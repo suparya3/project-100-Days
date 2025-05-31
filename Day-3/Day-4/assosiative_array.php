39d09a2777b2e4850434beef437f1fde10ea6401
suparya3 <ariyaharyanto361@gmail.com>
2025-05-31 15:48:54 +0700

    menambahkan file baru

---
+++ Day-3/Day-4/assosiative_array.php
@@ -0,0 +1,13 @@
+<?php
+// array
+//membuat array 
+// cara lama 
+$hari = array("senin", "selasa", "rabu");
+// cara baru
+$bulan =["januari", "february", "maret"];
+// array dengan beberapa tipe data
+$arr =["100", "seratus", true];
+
+// menampilkan array
+
+?>
